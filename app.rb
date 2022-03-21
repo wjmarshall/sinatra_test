8143badd232ebe6479157eebc18000187fd09c90
William Marshall <w.marshall@elsevier.com>
2022-03-21 16:00:08 +0000

    Require sinatra/reloader

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require "sinatra"
+require "sinatra/reloader" if development?
 
 get '/' do
   "Hello World!"
+end
+
+get '/secret' do
+  "Changed message"
+end
+
+get '/ruby' do
+  "Ruby is fun!"
+end
+
+get '/sinatra' do
+  "We love sinatra"
 end
