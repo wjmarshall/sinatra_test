28393f0f85aa6bb140d9ffb03046b70868e7ff21
William Marshall <w.marshall@elsevier.com>
2022-03-22 17:33:25 +0000

    Add views/form.erb

--- app.rb
+++ app.rb
@@ -22,8 +22,13 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+get '/form' do
+  erb(:form)
+end
+
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
 end
+
